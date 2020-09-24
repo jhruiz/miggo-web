9862ced032cc329626534ec8300063481b583fac
Jaiber <jaiber.ruiz@correounivalle.edu.co>
2020-09-24 13:21:25 -0500

    se realiza ajuste en reportes controller

--- app/Controller/ReportesController.php
+++ app/Controller/ReportesController.php
@@ -1089,7 +1089,7 @@ class ReportesController extends AppController {
             'Existencia M&aacute;xima',
         );
         $this->set(compact('productos'));
-        $this->set('titulos', $arr_titulos, );
+        $this->set('titulos', $arr_titulos);
         $this->render('export_xls', 'export_xls');
     }
 
