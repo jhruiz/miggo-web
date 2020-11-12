32b134699a111da6b415078eccdf28ac36289b2b
Jaiber <jaiber.ruiz@correounivalle.edu.co>
2020-11-12 00:15:05 -0500

    segunda prueba de subida a github

--- app/Controller/ReportesController.php
+++ app/Controller/ReportesController.php
@@ -5,7 +5,7 @@ App::uses('CuentasclientesController', 'Controller');
 
 class ReportesController extends AppController
 {
-    // obtiene estadisticas
+    // obtiene estadisticas r
     public function estadisticastortas()
     {
         $this->loadModel('Deposito');
