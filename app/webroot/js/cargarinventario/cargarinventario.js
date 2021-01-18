43fe7993d7ec47b0360c1da2e10427234c44a15a
Jaiber <jaiber.ruiz@correounivalle.edu.co>
2021-01-18 09:31:46 -0500

    se cambia la etiqueta del costo para proveedores regimen comun y regimen simplificado

--- app/webroot/js/cargarinventario/cargarinventario.js
+++ app/webroot/js/cargarinventario/cargarinventario.js
@@ -2,9 +2,9 @@ var tipoProveedor = function() {
     var type = $('#proveedore_id option:selected').data('type');
 
     if (type == '1') {
-        $('#val_prod').html('Costo Unitario (sin IVA)');
+        $('#val_prod').html('Costo Unitario Subtotal');
     } else {
-        $('#val_prod').html('Costo Unitario (IVA Incluido)');
+        $('#val_prod').html('Costo Unitario Total');
     }
 }
 
