aa67d5965bdf0e9b6e3742766223f52f4ec1199d
jhruiz <jaiber.ruiz@correounivalle.edu.co>
2026-06-24 22:57:15 -0500

    se ajuste función de impresión de factura en ticket

--- app/webroot/js/facturas/imprimirticketfast.js
+++ app/webroot/js/facturas/imprimirticketfast.js
@@ -73,27 +73,32 @@ var imprimirFacturaFacturaRapidaTicket = function() {
             iframe.style.width = '0px';
             iframe.style.height = '0px';
             iframe.style.border = 'none';
-            
-            document.body.appendChild(iframe);
-            
-            var mywindow = iframe.contentWindow || iframe.contentDocument;
-            if (mywindow.document) { mywindow = mywindow.document; }
-            
-            mywindow.open();
-            mywindow.write('<html><head><title>' + cliente.nombre + ' - Factura Electronica</title>');
-            mywindow.write('<style>');
-            mywindow.write('body { margin: 0; padding: 0; font-family: Arial, sans-serif; font-size: 11px; width: 80mm; color: #000; line-height: 1.3; }');
-            mywindow.write('@page { size: 80mm auto; margin: 0mm; }');
-            mywindow.write('@media print { body { margin: 0; width: 72mm; padding: 2mm; } .no-print { display: none; } }');
-            mywindow.write('table { width: 100%; border-collapse: collapse; margin-top: 5px; margin-bottom: 5px; }');
-            mywindow.write('th { font-size: 11px; padding: 5px 0; border-bottom: 1px solid #000; text-align: left; font-weight: bold; }');
-            mywindow.write('td { font-size: 11px; padding: 4px 0; vertical-align: top; }');
-            mywindow.write('.separador { border-top: 1px dashed #000; margin: 8px 0; }');
-            mywindow.write('.text-center { text-align: center; } .text-right { text-align: right; }');
-            mywindow.write('.logo-contenedor { width: 100%; text-align: center; font-size: 28px; font-family: "Courier New", Courier, monospace; letter-spacing: 4px; margin-bottom: 10px; }');
-            mywindow.write('</style></head><body>');
 
-            mywindow.write('<div style="width: 100%; padding-right: 5px;">');
+
+            //             // Abrir ventana limpia de impresión nativa
+            // var mywindow = window.open('', 'PRINT', 'height=600,width=400');
+            // mywindow.document.write('<html><head><title>' + cliente.nombre + ' - Documento de Venta</title>');
+            
+            // document.body.appendChild(iframe);
+            
+            // var mywindow = iframe.contentWindow || iframe.contentDocument;
+            // if (mywindow.document) { mywindow = mywindow.document; }
+
+            var mywindow = window.open('', 'PRINT', 'height=600,width=400');
+            mywindow.document.write('<html><head><title>' + cliente.nombre + ' - Factura Electronica</title>');
+            mywindow.document.write('<style>');
+            mywindow.document.write('body { margin: 0; padding: 0; font-family: Arial, sans-serif; font-size: 11px; width: 80mm; color: #000; line-height: 1.3; }');
+            mywindow.document.write('@page { size: 80mm auto; margin: 0mm; }');
+            mywindow.document.write('@media print { body { margin: 0; width: 72mm; padding: 2mm; } .no-print { display: none; } }');
+            mywindow.document.write('table { width: 100%; border-collapse: collapse; margin-top: 5px; margin-bottom: 5px; }');
+            mywindow.document.write('th { font-size: 11px; padding: 5px 0; border-bottom: 1px solid #000; text-align: left; font-weight: bold; }');
+            mywindow.document.write('td { font-size: 11px; padding: 4px 0; vertical-align: top; }');
+            mywindow.document.write('.separador { border-top: 1px dashed #000; margin: 8px 0; }');
+            mywindow.document.write('.text-center { text-align: center; } .text-right { text-align: right; }');
+            mywindow.document.write('.logo-contenedor { width: 100%; text-align: center; font-size: 28px; font-family: "Courier New", Courier, monospace; letter-spacing: 4px; margin-bottom: 10px; }');
+            mywindow.document.write('</style></head><body>');
+
+            mywindow.document.write('<div style="width: 100%; padding-right: 5px;">');
             
             // --- ENCABEZADO ---
             // Tratamiento del contenedor de imagen para mitigar errores de carga o rutas rotas
@@ -101,21 +106,21 @@ var imprimirFacturaFacturaRapidaTicket = function() {
             if (rawImgHtml.trim() !== "") {
                 // Inyectamos dinámicamente el controlador onerror para ocultar el recuadro si falla la imagen
                 var fixedImgHtml = rawImgHtml.replace('<img', '<img onerror="this.style.display=\'none\';"');
-                mywindow.write('<div>' + fixedImgHtml + '</div>');
+                mywindow.document.write('<div>' + fixedImgHtml + '</div>');
             }
-            mywindow.write('<div class="text-center"><b>' + empresa.nombre + '</b></div>');
-            mywindow.write('<div class="text-center" style="font-size: 10px;">NIT: ' + empresa.nit + ' - ' + empresa.texto1 + '</div>');
-            mywindow.write('<div class="text-center" style="font-size: 10px;">Resolución No. ' + resolucion.resolucion + ' de ' + resolucion.fechainicio + '</div>');
-            mywindow.write('<div class="text-center" style="font-size: 10px;">Prefijo: ' + resolucion.prefijo + ', Rango ' + resolucion.resolucioninicia + ' al ' + resolucion.resolucionfin + '</div>');
-            mywindow.write('<div class="text-center" style="font-size: 10px;">Vigencia Desde:' + resolucion.fechainicio + ' Hasta: ' + resolucion.fechafin + '</div>');
-            mywindow.write('<div class="text-center"><b>REPRESENTACIÓN GRÁFICA DE FACTURA ELECTRÓNICA</b></div>');
-            mywindow.write('<div class="text-center">' + empresa.direccion + ' - </div>');
-            mywindow.write('<div class="text-center">Tel: ' + empresa.telefono1 + ' | E-mail: ' + empresa.email + '</div>');
+            mywindow.document.write('<div class="text-center"><b>' + empresa.nombre + '</b></div>');
+            mywindow.document.write('<div class="text-center" style="font-size: 10px;">NIT: ' + empresa.nit + ' - ' + empresa.texto1 + '</div>');
+            mywindow.document.write('<div class="text-center" style="font-size: 10px;">Resolución No. ' + resolucion.resolucion + ' de ' + resolucion.fechainicio + '</div>');
+            mywindow.document.write('<div class="text-center" style="font-size: 10px;">Prefijo: ' + resolucion.prefijo + ', Rango ' + resolucion.resolucioninicia + ' al ' + resolucion.resolucionfin + '</div>');
+            mywindow.document.write('<div class="text-center" style="font-size: 10px;">Vigencia Desde:' + resolucion.fechainicio + ' Hasta: ' + resolucion.fechafin + '</div>');
+            mywindow.document.write('<div class="text-center"><b>REPRESENTACIÓN GRÁFICA DE FACTURA ELECTRÓNICA</b></div>');
+            mywindow.document.write('<div class="text-center">' + empresa.direccion + ' - </div>');
+            mywindow.document.write('<div class="text-center">Tel: ' + empresa.telefono1 + ' | E-mail: ' + empresa.email + '</div>');
             
-            mywindow.write('<div class="separador"></div>');
+            mywindow.document.write('<div class="separador"></div>');
             
             // --- INFO FACTURA ---
-            mywindow.write('<div class="text-center" style="font-size: 12px;"><b>FACTURA ELECTRONICA ' + facturaF.prefijo + ' ' + facturaF.consecutivodian + '</b></div>');
+            mywindow.document.write('<div class="text-center" style="font-size: 12px;"><b>FACTURA ELECTRONICA ' + facturaF.prefijo + ' ' + facturaF.consecutivodian + '</b></div>');
             
             // Formatear Fecha (Asumiendo formato: "2026-06-15 16:15:07")
             var fechaPartes = facturaF.created.split(' ');
@@ -123,32 +128,32 @@ var imprimirFacturaFacturaRapidaTicket = function() {
             var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
             var fechaTexto = ciudad.nombre + ' (' + depto.descripcion + '), ' + meses[parseInt(f[1])-1] + ' ' + f[2] + ', ' + f[0] + ' ' + fechaPartes[1];
             
-            mywindow.write('<div class="text-center">' + fechaTexto + '</div>');
-            mywindow.write('<div class="text-center">Emitido a la DIAN</div>');
+            mywindow.document.write('<div class="text-center">' + fechaTexto + '</div>');
+            mywindow.document.write('<div class="text-center">Emitido a la DIAN</div>');
             
-            mywindow.write('<div class="separador"></div>');
+            mywindow.document.write('<div class="separador"></div>');
             
             // --- DATOS DEL CLIENTE Y VENTA ---
-            mywindow.write('<div><b>Cliente:</b> ' + cliente.nombre + '</div>');
-            mywindow.write('<div><b>CC/NIT:</b> ' + cliente.nit + '</div>');
-            mywindow.write('<div><b>Tel:</b> ' + (cliente.telefono || '') + '</div>');
-            mywindow.write('<div><b>Dir:</b> ' + (cliente.direccion || '') + '</div>');
-            mywindow.write('<div><b>Orden:</b> | <b>Fecha:</b> </div>');
-            mywindow.write('<div><b>Vendedor:</b> ' + vendedor.nombre + '</div>');
+            mywindow.document.write('<div><b>Cliente:</b> ' + cliente.nombre + '</div>');
+            mywindow.document.write('<div><b>CC/NIT:</b> ' + cliente.nit + '</div>');
+            mywindow.document.write('<div><b>Tel:</b> ' + (cliente.telefono || '') + '</div>');
+            mywindow.document.write('<div><b>Dir:</b> ' + (cliente.direccion || '') + '</div>');
+            mywindow.document.write('<div><b>Orden:</b> | <b>Fecha:</b> </div>');
+            mywindow.document.write('<div><b>Vendedor:</b> ' + vendedor.nombre + '</div>');
             
             // Métodos de Pago
-            mywindow.write('<div><b>Método(s) de Pago:</b></div>');
+            mywindow.document.write('<div><b>Método(s) de Pago:</b></div>');
             var totalFacturaAcumulado = 0;
             $.each(data.tiposPagos, function(i, pago) {
                 var valorPago = parseFloat(pago.FacturaCuentaValore.valor) || 0;
                 totalFacturaAcumulado += valorPago;
-                mywindow.write('<div> - ' + pago.T.descripcion + ': $' + valorPago.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</div>');
+                mywindow.document.write('<div> - ' + pago.T.descripcion + ': $' + valorPago.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</div>');
             });
             
             // --- TABLA DE DETALLES ---
-            mywindow.write('<table><thead>');
-            mywindow.write('<tr><th style="width:65%;">DESCRIPCIÓN</th><th class="text-center" style="width:10%;">CANT</th><th class="text-right" style="width:25%;">TOTAL</th></tr>');
-            mywindow.write('</thead><tbody>');
+            mywindow.document.write('<table><thead>');
+            mywindow.document.write('<tr><th style="width:65%;">DESCRIPCIÓN</th><th class="text-center" style="width:10%;">CANT</th><th class="text-right" style="width:25%;">TOTAL</th></tr>');
+            mywindow.document.write('</thead><tbody>');
 
             var subtotalProd = 0;
             var totalIva = 0;
@@ -165,49 +170,59 @@ var imprimirFacturaFacturaRapidaTicket = function() {
                 subtotalProd += (valores.valorBaseUnitario * cant);
                 totalIva += (valores.valorIVA * cant);
 
-                mywindow.write('<tr>');
-                mywindow.write('<td>' + prod.descripcion + '<br><span style="font-size:10px; color:#555;">' + prod.codigo + '</span></td>');
-                mywindow.write('<td class="text-center">' + cant + '</td>');
-                mywindow.write('<td class="text-right">' + totalFila.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</td>');
-                mywindow.write('</tr>');
+                mywindow.document.write('<tr>');
+                mywindow.document.write('<td>' + prod.descripcion + '<br><span style="font-size:10px; color:#555;">' + prod.codigo + '</span></td>');
+                mywindow.document.write('<td class="text-center">' + cant + '</td>');
+                mywindow.document.write('<td class="text-right">' + totalFila.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</td>');
+                mywindow.document.write('</tr>');
             });
             
-            mywindow.write('</tbody></table>');
-            mywindow.write('<div class="separador"></div>');
+            mywindow.document.write('</tbody></table>');
+            mywindow.document.write('<div class="separador"></div>');
             
             // --- TOTALES ---
-            mywindow.write('<table style="margin-top:0px; width:100%;">');
-            mywindow.write('<tr><td class="text-right" style="width:60%;">Subtotal Serv:</td><td class="text-right" style="width:40%;">0.00</td></tr>');
-            mywindow.write('<tr><td class="text-right">Subtotal Prod:</td><td class="text-right">' + subtotalProd.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</td></tr>');
-            mywindow.write('<tr><td class="text-right"><b>Subtotal:</b></td><td class="text-right"><b>' + subtotalProd.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</b></td></tr>');
-            mywindow.write('<tr><td class="text-right">Descuento:</td><td class="text-right">0.00</td></tr>');
-            mywindow.write('<tr><td class="text-right"><b>Total Bruto:</b></td><td class="text-right"><b>' + subtotalProd.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</b></td></tr>');
-            mywindow.write('<tr><td class="text-right">IVA:</td><td class="text-right">' + totalIva.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</td></tr>');
-            mywindow.write('<tr><td class="text-right">INC:</td><td class="text-right">0.00</td></tr>');
-            mywindow.write('<tr><td class="text-right">Bolsas:</td><td class="text-right">0.00</td></tr>');
-            mywindow.write('<tr><td class="text-right"><b>Total Impuestos:</b></td><td class="text-right"><b>' + totalIva.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</b></td></tr>');
-            mywindow.write('<tr><td class="text-right" style="font-size:12px; border-top:1px solid #000; padding-top:4px;"><b>TOTAL FACTURA:</b></td><td class="text-right" style="font-size:12px; border-top:1px solid #000; padding-top:4px;"><b>$' + totalFacturaAcumulado.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</b></td></tr>');
-            mywindow.write('</table>');
+            mywindow.document.write('<table style="margin-top:0px; width:100%;">');
+            mywindow.document.write('<tr><td class="text-right" style="width:60%;">Subtotal Serv:</td><td class="text-right" style="width:40%;">0.00</td></tr>');
+            mywindow.document.write('<tr><td class="text-right">Subtotal Prod:</td><td class="text-right">' + subtotalProd.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</td></tr>');
+            mywindow.document.write('<tr><td class="text-right"><b>Subtotal:</b></td><td class="text-right"><b>' + subtotalProd.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</b></td></tr>');
+            mywindow.document.write('<tr><td class="text-right">Descuento:</td><td class="text-right">0.00</td></tr>');
+            mywindow.document.write('<tr><td class="text-right"><b>Total Bruto:</b></td><td class="text-right"><b>' + subtotalProd.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</b></td></tr>');
+            mywindow.document.write('<tr><td class="text-right">IVA:</td><td class="text-right">' + totalIva.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</td></tr>');
+            mywindow.document.write('<tr><td class="text-right">INC:</td><td class="text-right">0.00</td></tr>');
+            mywindow.document.write('<tr><td class="text-right">Bolsas:</td><td class="text-right">0.00</td></tr>');
+            mywindow.document.write('<tr><td class="text-right"><b>Total Impuestos:</b></td><td class="text-right"><b>' + totalIva.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</b></td></tr>');
+            mywindow.document.write('<tr><td class="text-right" style="font-size:12px; border-top:1px solid #000; padding-top:4px;"><b>TOTAL FACTURA:</b></td><td class="text-right" style="font-size:12px; border-top:1px solid #000; padding-top:4px;"><b>$' + totalFacturaAcumulado.toLocaleString('es-CO', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + '</b></td></tr>');
+            mywindow.document.write('</table>');
             
-            mywindow.write('<div class="separador"></div>');
+            mywindow.document.write('<div class="separador"></div>');
             
             // --- PIE DE PÁGINA ---
-            mywindow.write('<div class="text-center" style="font-size: 9px; line-height: 1.2;">Software de facturación suministrado por Miggo Solutions S.A.S<br>NIT 901629169</div>');
-            mywindow.write('<div class="text-center" style="font-size: 9px; line-height: 1.2;">Modalidad software adquirido desarrollador habilitado por la DIAN según concepto 013246 de 2025</div>');
-            mywindow.write('<div class="text-center" style="font-size: 9px; line-height: 1.2;">www.miggo.com.co línea de servicio al cliente 3116871320.</div>');
+            mywindow.document.write('<div class="text-center" style="font-size: 9px; line-height: 1.2;">Software de facturación suministrado por Miggo Solutions S.A.S<br>NIT 901629169</div>');
+            mywindow.document.write('<div class="text-center" style="font-size: 9px; line-height: 1.2;">Modalidad software adquirido desarrollador habilitado por la DIAN según concepto 013246 de 2025</div>');
+            mywindow.document.write('<div class="text-center" style="font-size: 9px; line-height: 1.2;">www.miggo.com.co línea de servicio al cliente 3116871320.</div>');
             
-            mywindow.write('</div>');
-            mywindow.write('</body></html>');
-            
+            mywindow.document.write('</div>');
+            mywindow.document.write('</body></html>');
+
+
+
+            // Ejecución de impresión limpia
+            mywindow.focus();
             setTimeout(function() {
-                iframe.contentWindow.focus();
-                iframe.contentWindow.print();
+                mywindow.print();
+                mywindow.close();
+                location.reload();
+            }, 250);
+            
+            // setTimeout(function() {
+            //     iframe.contentWindow.focus();
+            //     iframe.contentWindow.print();
                 
-                setTimeout(function() {
-                    $('#' + iframeId).remove(); 
-                    location.reload();
-                }, 400);
-            }, 300);
+            //     setTimeout(function() {
+            //         $('#' + iframeId).remove(); 
+            //         location.reload();
+            //     }, 400);
+            // }, 300);
         },
         error: function(xhr, status, error) {
             console.error("Error al recuperar los datos de la factura: ", error);
